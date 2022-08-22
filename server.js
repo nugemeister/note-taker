32227ca14cc1e20cad5ba0c11773f226a2dd43c2
Ali Nugen <alexandra.nugen@gmail.com>
2022-08-21 17:02:32 -0700

    updating server.js file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const apiRoutes = require('./routes/');
+const path = require('path');
+
+const app = express ();
+
+const PORT = process.env.PORT || 3001;
+
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
+
+// api routes
+app.use( '/api', apiRouter );
+
+// reference static public folder
+app.use( express.static( 'public' ) );
+
+// reference default index.html
+app.get( '*', ( req, res ) => {
+    res.sendFile( path.join( __dirname, '/public/index.html' ) );
+} );
+
+// reference notes page
+app.get( '/notes', ( req, res ) => {
+    res.sendFile( path.join( __dirname, '/public/notes.html' ) );
+} );
+
+// listen for server request
+app.listen( PORT, () => {
+    console.log( `App listening on port ${PORT}` );
+} );
