c263bde70dddb05957f93d1cf11fed70d6b29ca3
Ali Nugen <alexandra.nugen@gmail.com>
2022-08-21 20:37:26 -0700

    updating server and routes files

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ const app = express ();
 
 const PORT = process.env.PORT || 3001;
 
-app.use( express.json() );
-app.use( express.urlencoded( { extended: true } ) );
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 // api routes
-app.use( '/api', apiRouter );
+app.use('/api', apiRoutes);
 
 // reference static public folder
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
 
 // reference default index.html
-app.get( '*', ( req, res ) => {
-    res.sendFile( path.join( __dirname, '/public/index.html' ) );
-} );
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+});
 
 // reference notes page
-app.get( '/notes', ( req, res ) => {
-    res.sendFile( path.join( __dirname, '/public/notes.html' ) );
-} );
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname,'/public/notes.html'));
+});
 
 // listen for server request
-app.listen( PORT, () => {
-    console.log( `App listening on port ${PORT}` );
-} );
+app.listen(PORT, () => {
+    console.log(`Application is listening on port ${PORT}`);
+});
